ef917ce2609ca17a4ea9de83c98229ee9bb65279
Anastasiia <musienkoanastya@gmail.com>
2025-04-08 00:27:14 +0300

    VERSION 1

--- src/main/java/com/anastasiia/spring_musiienko/SpringMusiienkoApplication.java
+++ src/main/java/com/anastasiia/spring_musiienko/SpringMusiienkoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMusiienkoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMusiienkoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMusiienkoApplication.class, args);
+    }
 
 }
