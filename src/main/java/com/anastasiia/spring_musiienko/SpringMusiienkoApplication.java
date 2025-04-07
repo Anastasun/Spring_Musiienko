d38d6ea6bc3e959f3b3641068d109fcc9a750a79
Anastasiia <musienkoanastya@gmail.com>
2025-04-07 23:19:40 +0300

    Initial commit

---
+++ src/main/java/com/anastasiia/spring_musiienko/SpringMusiienkoApplication.java
@@ -0,0 +1,13 @@
+package com.anastasiia.spring_musiienko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMusiienkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMusiienkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anastasiia/spring_musiienko/SpringMusiienkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastasiia.spring_musiienko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMusiienkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
